bc606542129ad3869231992e9ee6b32d433ffbbf
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2024-07-25 12:45:44 +0100

    Add scripts

---
+++ src/experiments.sh
@@ -0,0 +1,4 @@
+python main.py model=continuous dataset=cora
+python main.py model=continuous dataset=facebook
+python main.py model=continuous dataset=citeseer
+python main.py model=continuous dataset=wiki
